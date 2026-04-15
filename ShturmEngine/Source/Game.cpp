2648a5bcf1c2c816152d54fceef7206bf89ecaea
Bryzghalov Maksym <2001bruzga2001@gmail.com>
2026-04-15 11:55:42 +0300

    [GameWindow] Added game class and basic prototype of it with concept of game loop

---
+++ ShturmEngine/Source/Game.cpp
@@ -0,0 +1,43 @@
+#include "Game.h"
+
+#include <iostream>
+
+Game::Game()
+{
+	std::cout << "Game constructor called!" << std::endl;
+}
+
+Game::~Game()
+{
+	std::cout << "Game destructor called!" << std::endl;
+}
+
+void Game::Initialize()
+{
+}
+
+void Game::Run()
+{
+}
+
+void Game::ProcessInput()
+{
+	while (true)
+	{
+		ProcessInput();
+		Update();
+		Render();
+	}
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
+
+void Game::Destroy()
+{
+}
--- ShturmEngine/Source/Main.cpp
+++ ShturmEngine/Source/Main.cpp
@@ -1,24 +1,12 @@
-#include <iostream>
-#include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <SDL3_mixer/SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main() {
-    // Simply tries to use the Lua language with the Sol library
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
+int main(int argc, char* argv[])
+{
+	Game game;
 
-    // Tries to create a vector2 and normalize it with the GLM library
-    glm::vec2 velocity = glm::vec2(5.0, -2.5);
-    velocity = glm::normalize(velocity);
+	game.Initialize();
+	game.Run();
+	game.Destroy();
 
-    // Tries to initialize SDL
-    SDL_Init((SDL_INIT_VIDEO));
-
-    std::cout << "Yay! Dependencies work correctly." << std::endl;
     return 0;
 }
