74e64dba64051b40a0503db11252182023ad35b1
Bryzghalov Maksym <2001bruzga2001@gmail.com>
2026-04-25 13:55:05 +0300

    [ECS] Defined basic classes and component signature

--- Engine/ECS/ECS.cpp
+++ Engine/ECS/ECS.cpp
@@ -1,1 +1,28 @@
 #include "ECS.h"
+
+Entity::Entity(int id) : _id(id)
+{
+}
+
+int Entity::GetId() const
+{
+	return _id;
+}
+
+void System::AddEntityToSystem(Entity entity)
+{
+}
+
+void System::RemoveEntityFromSystem(Entity entity)
+{
+}
+
+std::vector<Entity> System::GetSystemEntities() const
+{
+	return _entities;
+}
+
+Signature System::GetComponentSignature() const
+{
+	return _componentSignature;
+}
--- Engine/ECS/ECS.h
+++ Engine/ECS/ECS.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENTS = 32;
+/////////////////////////////////////////////////////////////////////
+// Signature
+/////////////////////////////////////////////////////////////////////
+// We use a bitset (1s and 0s) to keep track of which components
+// and entity has and also helps keep track of which entities
+// a system is interested in.
+/////////////////////////////////////////////////////////////////////
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
 class Component
 {
 	// TODO: ...
@@ -7,12 +20,34 @@ class Component
 
 class Entity
 {
-	// TODO: ...
+public:
+	Entity(int id);
+
+	int GetId() const;
+
+private:
+	int _id;
 };
 
+/////////////////////////////////////////////////////////////////////
+// System
+/////////////////////////////////////////////////////////////////////
+// The system processes entities that contain a specific signature
+/////////////////////////////////////////////////////////////////////
 class System 
 {
-	// TODO: ...
+public:
+	System() = default;
+	~System() = default;
+
+	void AddEntityToSystem(Entity entity);
+	void RemoveEntityFromSystem(Entity entity);
+	std::vector<Entity> GetSystemEntities() const;
+	Signature GetComponentSignature() const;
+
+private:
+	Signature _componentSignature;
+	std::vector<Entity> _entities;
 };
 
 class Registry
